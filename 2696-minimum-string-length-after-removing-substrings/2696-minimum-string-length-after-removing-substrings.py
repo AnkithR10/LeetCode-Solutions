135550a2c12cf1ac51b0abee040c33c0c0c404b0
Ankith R <ankith.ravishankar@gmail.com>
2026-07-10 11:43:46 +0530

    Time: 12 ms (41.97%), Space: 12.3 MB (51.81%) - LeetHub

---
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def minLength(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        stack = []
+        
+        for char in s:
+            # Check if we have a match to remove
+            if stack and ((stack[-1] == 'A' and char == 'B') or 
+                          (stack[-1] == 'C' and char == 'D')):
+                stack.pop()
+            else:
+                stack.append(char)
+                
+        return len(stack)
